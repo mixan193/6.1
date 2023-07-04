f41f21a11f9549100de8f2f4dc2a45a128a89ce1
Mihael Kardash <mixan193@gmail.com>
2023-07-04 13:54:28 +0300

    Добавьте файлы проекта.

---
+++ 6.1/DBHandler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._1
+{
+    internal class DBHandler
+    {
+        private string pathToDBFile;
+        private int employersCount;
+        public int EmployersCount
+        {
+            get;
+            private set;
+        }
+        public DBHandler(string pathToDBFile)
+        {
+            this.pathToDBFile = pathToDBFile;
+            if (!File.Exists(pathToDBFile))
+            {
+                FileStream fs = new FileStream(pathToDBFile, FileMode.Create);
+                fs.Close();
+                employersCount = 0;
+            }
+            else
+            {
+                employersCount = GetLastID();
+            }
+        }
+
+        public int GetLastID()
+        {
+            return File.ReadLines(pathToDBFile).Count();
+        }
+
+
+        public bool AddEmployerToDB(string employer)
+        {
+            try
+            {
+                StreamWriter streamWriter = new StreamWriter(pathToDBFile, true);
+                streamWriter.WriteLine(employer);
+                streamWriter.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ 6.1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DBHandler dBHandler = new DBHandler("DB.txt");
+            int id;
+            DateTime dateTime;
+            string fullName;
+            int height;
+            DateTime dateOfBirth;
+            string placeOfBirth;
+            while (true)
+            {
+                id = dBHandler.GetLastID();
+                dateTime = DateTime.Now;
+                Console.WriteLine("Введите ФИО ");
+                fullName = EnterString();
+                Console.WriteLine("Введите рост ");
+                height = EnterInt();
+                Console.WriteLine("Введите дату рождения ");
+                dateOfBirth = EnterDate();
+                Console.WriteLine("Введите место рождения ");
+                placeOfBirth = EnterString();
+                Employer employer = new Employer(id, dateTime, fullName, height, dateOfBirth, placeOfBirth);
+                dBHandler.AddEmployerToDB(employer.EmployerToString());
+            }
+        }
+
+        private static string EnterString()
+        {
+            string result;
+            while ((result = Console.ReadLine()) == string.Empty)
+            {
+                Console.WriteLine("Вы ничего не ввели");
+            }
+            return result;
+        }
+
+        private static int EnterInt()
+        {
+            int result = 0;
+            while(result < 1)
+            {
+                try
+                {
+                    result = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Введите число");
+                }
+            }
+            return result;
+        }
+
+        private static DateTime EnterDate()
+        {
+            DateTime result = new DateTime();
+            string dateOfBirdthInString;
+            bool isAccepted = false;
+            while (!isAccepted)
+            {
+                try
+                {
+                    dateOfBirdthInString = Console.ReadLine();
+                    string[] strings = dateOfBirdthInString.Split('.');
+                    result = new DateTime(int.Parse(strings[2]), int.Parse(strings[1]), int.Parse(strings[0]));
+                    isAccepted = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Введите дату в формате день.месяц.год");
+                }
+            }
+            return result;
+        }
+
+    }
+}
